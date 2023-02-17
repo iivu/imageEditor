a74a5cc816e3ddd500512d3b505507ec5914d7ac
Leo Cheung <leozhang621@gmail.com>
2023-02-17 16:54:08 +0800

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from '@rollup/plugin-typescript';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+  input: 'src/index.ts',
+  output: [
+    { format: 'cjs', file: 'dist/index.cjs.js' },
+    { format: 'esm', file: 'dist/index.esm.js' },
+    { format: 'umd', file: 'dist/index.umd.js', name: 'iivu' },
+  ],
+  plugins: [
+    typescript(),
+  ],
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+export type ImageEditorConfig = {
+  backgroundColor?: string;
+};
+
+export class ImageEditor {
+  private canvas: HTMLCanvasElement | null;
+  private image: HTMLImageElement | null;
+  private ctx: CanvasRenderingContext2D | null;
+  private width = 0;
+  private height = 0;
+  private editInfo = { x: 0, y: 0, scale: 1, rotate: 0 };
+  private config: ImageEditorConfig = {};
+
+  constructor(canvas: HTMLCanvasElement, image: HTMLImageElement, config: ImageEditorConfig = {}) {
+    this.canvas = canvas;
+    this.image = image;
+    this.config = config;
+    this.ctx = canvas.getContext("2d")!;
+    this.ctx.imageSmoothingEnabled = true;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.editInfo = { x: this.width / 2, y: this.height / 2, scale: 1, rotate: 0 };
+    this.update();
+  }
+
+  update() {
+    if (this.ctx === null || this.image == null) return;
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.ctx.save();
+    if (this.config.backgroundColor) {
+      this.ctx.fillStyle = this.config.backgroundColor;
+      this.ctx.fillRect(0, 0, this.width, this.height);
+      this.ctx.restore();
+    }
+    // 画布原点设置在canvas中心
+    this.ctx.transform(this.editInfo.scale, 0, 0, this.editInfo.scale, this.editInfo.x, this.editInfo.y);
+    this.ctx.rotate((this.editInfo.rotate * Math.PI) / 180);
+    // 保证宽度不发生裁剪
+    const imageRatio = this.image.width / this.image.height;
+    const imageDrawWidth = Math.min(this.height * imageRatio, this.width);
+    const imageDrawHeight = imageDrawWidth / imageRatio;
+    this.ctx.drawImage(this.image, -this.width / 2, -this.height / 2, imageDrawWidth, imageDrawHeight);
+    this.ctx.restore();
+  }
+
+  move(x: number, y: number) {
+    this.editInfo.x = x;
+    this.editInfo.y = y;
+    this.update();
+  }
+
+  pinch(scale: number) {
+    this.editInfo.scale = Math.max(0.2, scale);
+    this.update();
+  }
+
+  rotate(r: number) {
+    this.editInfo.rotate = r;
+    this.update();
+  }
+
+  getRotate() {
+    return this.editInfo.rotate;
+  }
+
+  getScale() {
+    return this.editInfo.scale;
+  }
+
+  getPosition() {
+    return { x: this.editInfo.x, y: this.editInfo.y };
+  }
+
+  toDataURL() {
+    return this.canvas!.toDataURL("image/jpeg", 0.8);
+  }
+
+  destroy() {
+    this.canvas = null;
+    this.image = null;
+  }
+}
